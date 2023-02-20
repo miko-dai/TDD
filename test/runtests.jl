23fdf1c555b2f34bd8ac5e726555b85ae03faf08
miko-dai <miko.yc.dai@gmail.com>
2023-02-20 11:23:02 -0600

    Adding test for directly-connected neighbors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using TDD
 using Test
 
+testgraph = [
+          [2, 3],     # list of nodes that can be reached from node 1
+          [1],        # list of nodes that can be reached from node 2
+          [1],        # ", node 3
+          [5],        # ", node 4
+          [4]]        # ", node 5
+
 @testset "TDD.jl" begin
-    # Write your tests here.
+    find_neighbors(testgraph, 1) == [1,2,3]
 end
