3517a9b4f613373b5b605c9ca9c86cc29160dbec
miko-dai <miko.yc.dai@gmail.com>
2023-02-20 18:30:12 -0600

    Adding reachable_nodes function

--- src/TDD.jl
+++ src/TDD.jl
@@ -1,4 +1,5 @@
 module TDD
 export direct_neighbors
+export reachable_nodes
 include("functions.jl")
 end
--- src/functions.jl
+++ src/functions.jl
@@ -6,3 +6,15 @@ function direct_neighbors(graph, i::Integer)
     sort!(append!(neighbors, i)) # the node is connected to itself
     return(neighbors)
 end
+
+function reachable_nodes(graph, i::Integer)
+    neighbors = Set(direct_neighbors(graph, i))
+    reachable = Set()
+    while reachable != neighbors
+        reachable = copy(neighbors)
+        for n in reachable
+            push!(neighbors, direct_neighbors(graph, n))
+        end
+    end
+    return collect(reachable)
+end
