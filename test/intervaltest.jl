9026e66f381878905fd84c5ad2bf1af33e83ef29
miko-dai <miko.yc.dai@gmail.com>
2023-02-23 12:07:15 -0600

    Adding test for subset

--- test/intervaltest.jl
+++ test/intervaltest.jl
@@ -10,4 +10,7 @@ iv = Interval(3, 5)
     @test 3.59 ∈ iv
     @test isempty(iv) == false
     @test isempty(Interval(3,2)) == true
+    @test iv ⊆ iv
+    @test [3, 3.5, 4] ⊆ iv
+    @test Interval(3,2) ⊆ iv
 end
