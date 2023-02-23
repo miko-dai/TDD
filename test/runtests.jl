9abdf864617249e27cce53bf56748000dbacc766
miko-dai <miko.yc.dai@gmail.com>
2023-02-22 22:07:25 -0600

    Adding tests to adapt for adjacency matrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,8 +9,19 @@ testgraph = [
           [5],        # ", node 4
           [4]]        # ", node 5
 
+A = Bool[
+  1 1 1 0 0;
+  1 1 0 0 0;
+  1 0 1 0 0;
+  0 0 0 1 1;
+  0 0 0 1 1;
+]
+
 @testset "TDD.jl" begin
     @test direct_neighbors(testgraph, 1) == [1,2,3]
     @test reachable_nodes(testgraph, 2) == [1,2,3]
     @test find_components(testgraph) == [[1,2,3],[4,5]]
+    @test direct_neighbors(A, 1) == [1,2,3]
+    @test reachable_nodes(A, 2) == [1,2,3]
+    @test find_components(A) == [[1,2,3],[4,5]]
 end
